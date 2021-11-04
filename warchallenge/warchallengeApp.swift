552b315ee8e56b635f808bc9e9741376ca0b24c0
Oscar Montes Camberos <o.montescamberos@gmail.com>
2021-11-04 13:34:15 -0600

    Initial Commit

---
+++ warchallenge/warchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warchallengeApp.swift
+//  warchallenge
+//
+//  Created by Oscar Montes Camberos on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct warchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
